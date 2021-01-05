354a81d31805e3d3f9fc0c2c7db91da32d7a2538
zakirangwala <zakirangwala@gmail.com>
2021-01-05 15:33:25 -0500

    main component + styles

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import useStyles from "./styles";
+
+const Main = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Made by Zaki" />
+      <CardContent>
+        <Typography variant="h5" align="center">
+          Total Balance : $100
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        ></Typography>
+        <Divider />
+      </CardContent>
+      <CardContent className={classes.CardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}></Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
