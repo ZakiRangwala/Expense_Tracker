9a2d9648775e218615c8732dac3c20ddbd285497
zakirangwala <zakirangwala@gmail.com>
2021-01-05 15:21:14 -0500

    display grids

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Details from "./components/Details/Details";
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="App">
+      <Grid
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+            Main
+          <Details />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default App
+export default App;
