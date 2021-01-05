87a1175d30cc9e2246d522e61caa0553eff72330
zakirangwala <zakirangwala@gmail.com>
2021-01-05 16:37:35 -0500

    list components

---
+++ src/components/Main/List/List.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import useStyles from "./styles"
+import {List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide} from '@material-ui/core'
+import {Delete, MoneyOff} from '@material-ui/icons'
+
+const List = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default List
