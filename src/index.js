8a0437f591b881494a82719986a1015ed7cc3570
zakirangwala <zakirangwala@gmail.com>
2021-01-11 22:41:18 -0500

    speechly config

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import { SpeechProvider } from "@speechly/react-client";
 import { Provider } from "./context/context";
 
 ReactDOM.render(
+  <SpeechProvider appId="a6496470-11bf-4fd4-b130-2300a36fdd88" language="en-US">
   <Provider>
     <App />
-  </Provider>,
+  </Provider>
+  </SpeechProvider>,
   document.getElementById("root")
 );
