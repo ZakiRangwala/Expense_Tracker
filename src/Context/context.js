4767c7b251103994aa610cf723a5d0fd870d47b5
zakirangwala <zakirangwala@gmail.com>
2021-01-12 12:30:50 -0500

    local storage

--- src/Context/context.js
+++ src/Context/context.js
@@ -1,7 +1,22 @@
 import React, { useReducer, createContext } from "react";
 import contextReducer from "./contextReducer";
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem("transactions")) || [
+  {
+    amount: 500,
+    category: "Deposits",
+    date: "2021-01-19",
+    id: "36aa3b11-670c-49ac-8d7f-bf2d19a7e6e6",
+    type: "Income",
+  },
+  {
+    amount: 100,
+    category: "Phone",
+    date: "2021-01-19",
+    id: "36aa3b11-670c-49ac-8d1f-bf2d19a7e6e6",
+    type: "Expense",
+  },
+];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
@@ -16,9 +31,15 @@ export const Provider = ({ children }) => {
     dispatch({ type: "ADD_TRANSACTION", payload: transaction });
   };
 
+  const balance = transactions.reduce((acc, currVal) => {
+    return currVal.type === "Expense"
+      ? acc - currVal.amount
+      : acc + currVal.amount;
+  }, 0);
+
   return (
     <ExpenseTrackerContext.Provider
-      value={{ deleteTransaction, addTransaction, transactions}}
+      value={{ deleteTransaction, addTransaction, transactions, balance}}
     >
       {children}
     </ExpenseTrackerContext.Provider>
