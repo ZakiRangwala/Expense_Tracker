50316f653472e35384cba0670a20b37f39304117
zakirangwala <zakirangwala@gmail.com>
2021-01-12 12:58:17 -0500

    mobile styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import Details from "./components/Details/Details";
 import Main from "./components/Main/Main";
-import {PushToTalkButton, PushToTalkButtonContainer, ErrorPanel} from "@speechly/react-ui"
+import {
+  PushToTalkButton,
+  PushToTalkButtonContainer,
+  ErrorPanel,
+} from "@speechly/react-ui";
 import useStyles from "./styles";
 
 const App = () => {
@@ -18,13 +22,16 @@ const App = () => {
         justify="center"
         style={{ height: "100vh" }}
       >
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.last}>
           <Details title="Expense" />
         </Grid>
       </Grid>
